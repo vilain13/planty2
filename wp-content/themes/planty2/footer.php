98f67411b68f128b02638e34f323401b3c14ae73
Gill Guillermin <guillerg680@gmail.com>
2023-05-23 13:32:23 +0200

    Page Commander OK Responsive OK  Validator W3C OK

--- wp-content/themes/planty2/footer.php
+++ wp-content/themes/planty2/footer.php
@@ -1,9 +1,20 @@
-
-<html>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;600;800;900&family=Inter:wght@900&display=swap" rel="stylesheet">
+		<title>Riding Cities</title>
+	</head>
+<body>
 	<footer class="monFooter">
 		<p>
 			<a class="effetMonFooter" href="">Mentions légales</a>
 		</p>
 	</footer>
 	<?php wp_footer(); ?>
+</body>
 </html>
