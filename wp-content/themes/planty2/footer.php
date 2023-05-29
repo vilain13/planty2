13cc186c63a7b0a3cc554adbd7433148d72abe67
Gill Guillermin <guillerg680@gmail.com>
2023-05-29 16:45:40 +0200

    Projet Planty Prêt Soutenance

--- wp-content/themes/planty2/footer.php
+++ wp-content/themes/planty2/footer.php
@@ -3,16 +3,13 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;600;800;900&family=Inter:wght@900&display=swap" rel="stylesheet">
-		<title>Riding Cities</title>
+		<link rel="stylesheet" type="text/css" href="css/theme.css">
+		<title>Planty</title>
 	</head>
 <body>
 	<footer class="monFooter">
 		<p>
-			<a class="effetMonFooter" href="">Mentions légales</a>
+			<a class="effetMonFooter" href="http://localhost:8888/planty2/">Mentions légales</a>
 		</p>
 	</footer>
 	<?php wp_footer(); ?>
